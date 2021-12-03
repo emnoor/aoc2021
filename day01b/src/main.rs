22056b643935daef82ff04a6b8c1b016ceb4fdd1
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2021-12-03 23:08:49 +0600

    day 1 - part 2

---
+++ day01b/src/main.rs
@@ -0,0 +1,13 @@
+use itertools::Itertools;
+
+fn main() {
+    println!(
+        "{}",
+        include_str!("../input.txt")
+            .split_whitespace()
+            .map(|s| s.parse::<i64>().unwrap())
+            .tuple_windows()
+            .map(|(a, _, _, b)| if a < b { 1 } else { 0 })
+            .sum::<i64>()
+    );
+}
